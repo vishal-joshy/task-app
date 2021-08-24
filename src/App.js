64bc9ca3816b12ef510a7e9ac5fdc801d4774dd9
VishalJoshy <1812-vishal@protonmail.com>
2021-08-24 12:04:10 +0530

    Adds input form data in state using setState

--- src/App.js
+++ src/App.js
@@ -4,14 +4,34 @@ import Overview from './components/Overview';
 class App extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			inputValue: '',
+		};
+
+		this.onSubmitForm = this.onSubmitForm.bind(this);
+		this.onTaskInput = this.onTaskInput.bind(this);
 	}
+
+	onSubmitForm(e) {
+		e.preventDefault();
+		console.log(this.state.inputValue);
+	}
+	onTaskInput(e) {
+		this.setState({
+			inputValue: e.target.value,
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				<form>
 					<label>Input task Name</label>
-					<input type="text" placeholder="eg. Task 1"></input>
-					<button type="submit">Add Task</button>
+					<input type="text" placeholder="eg. Task 1" onChange={this.onTaskInput}></input>
+					<button type="submit" onClick={this.onSubmitForm}>
+						Add Task
+					</button>
 				</form>
 				<div>
 					<h3>Task List</h3>
