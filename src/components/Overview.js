cc106595347ee02cf60f81c0e7e217b9881191fa
VishalJoshy <1812-vishal@protonmail.com>
2021-08-24 11:52:44 +0530

    Initial commit

---
+++ src/components/Overview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Overview extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return <div>{this.props.title}</div>;
+	}
+}
+
+export default Overview;
